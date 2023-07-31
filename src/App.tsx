bd35d43c3dcbb59a78e27d2adfc690767bbc287e
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-31 01:40:19 -0400

    <INIT> creating new projec, ecommerce app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App (): JSX.Element {
+  return (
+    <>
+    <h1 className="text-2xl font-bold text-red-400"> Vite + React + Typescript + Tailwind </h1>
+    </>
+  )
+}
+
+export default App
