167cfa7b4f83c6eb47fa147ff2606601945d3834
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-31 23:02:55 -0400

    <FEAT> adding background image to sections

--- src/components/section/section.component.tsx
+++ src/components/section/section.component.tsx
@@ -1,11 +1,16 @@
 interface Props {
   sectionName: string
+  sectionImage: string
 }
 
-export const Section: React.FC<Props> = ({ sectionName }): JSX.Element => {
+export const Section: React.FC<Props> = ({ sectionName, sectionImage }): JSX.Element => {
+  const style = {
+    backgroundImage: `url(${sectionImage})`
+  }
+
   return (
     <div className="">
-      {/* <img src={} alt=''/> */}
+      <div style={style}></div>
       <div className="">
         <h2 className="text-2xl font-bold"> {sectionName} </h2>
         <p className="font-semibold"> SHOP NOW </p>
--- src/components/section_container/section_container.component.tsx
+++ src/components/section_container/section_container.component.tsx
@@ -1,12 +1,12 @@
 import { Section } from '../section/section.component.js'
-import { sections } from './section_container.const.js'
+import { sections } from './section_containerconst.js'
 
 export const SectionContainer: React.FC = (): JSX.Element => {
   return (
     <div>
       {
         sections.map(section => {
-          return <Section key={section.id} sectionName={section.name}/>
+          return <Section key={section.id} sectionName={section.title} sectionImage={section.imageUrl}/>
         })
       }
     </div>
