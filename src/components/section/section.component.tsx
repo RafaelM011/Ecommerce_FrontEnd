81adc6c18d6f3d022b3da33beb9c37fc800fc3f9
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-31 23:51:29 -0400

    <STYLE> styling the categories

--- src/components/section/section.component.tsx
+++ src/components/section/section.component.tsx
@@ -1,3 +1,5 @@
+// import Hats from '../../assets/hats.png'
+
 interface Props {
   sectionName: string
   sectionImage: string
@@ -5,15 +7,16 @@ interface Props {
 
 export const Section: React.FC<Props> = ({ sectionName, sectionImage }): JSX.Element => {
   const style = {
-    backgroundImage: `url(${sectionImage})`
+    backgroundImage: `url(${sectionImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
   }
 
   return (
-    <div className="">
-      <div style={style}></div>
-      <div className="">
-        <h2 className="text-2xl font-bold"> {sectionName} </h2>
-        <p className="font-semibold"> SHOP NOW </p>
+    <div className="w-3/12 min-w-[500px] h-[450px] flex justify-center items-center grow" style={style}>
+      <div className="w-[200px] h-[200px] border-white border-2 flex flex-col justify-center items-center">
+        <h2 className="text-2xl font-bold text-white"> {sectionName} </h2>
+        <p className="font-semibold text-white"> SHOP NOW </p>
       </div>
     </div>
   )
--- src/components/section_container/section_container.component.tsx
+++ src/components/section_container/section_container.component.tsx
@@ -1,9 +1,9 @@
 import { Section } from '../section/section.component.js'
-import { sections } from './section_containerconst.js'
+import { sections } from './section_container.const.js'
 
 export const SectionContainer: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <div className='flex flex-wrap gap-4 h-[900px]'>
       {
         sections.map(section => {
           return <Section key={section.id} sectionName={section.title} sectionImage={section.imageUrl}/>
