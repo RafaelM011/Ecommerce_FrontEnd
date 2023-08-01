69b7f91d9a854e7515003d00b416a341e1bfbe58
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-08-01 00:28:50 -0400

    <STYLE> transition takes longer

--- src/components/section/section.component.tsx
+++ src/components/section/section.component.tsx
@@ -14,12 +14,12 @@ export const Section: React.FC<Props> = ({ sectionName, sectionImage }): JSX.Ele
     backgroundImage: `url(${sectionImage})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
-    transition: '1s'
+    transition: '5s'
   }
 
   return (
     <div className="group relative w-3/12 min-w-[500px] h-[450px] flex justify-center items-center grow overflow-hidden cursor-pointer">
-      <div style={style} className='hover:scale-105'></div>
+      <div style={style} className='hover:scale-[110%]'></div>
       <div className="group-hover:opacity-[0.99] opacity-80 w-[200px] h-[200px] bg-white border-black border-4 flex flex-col justify-evenly items-center">
         <h2 className="text-2xl font-bold text-slate-500"> {sectionName} </h2>
         <p className="font-semibold text-slate-500"> SHOP NOW </p>
