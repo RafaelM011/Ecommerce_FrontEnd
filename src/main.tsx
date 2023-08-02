8520f30495903577a6b62d438d8e40fe7c84f436
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-08-01 23:50:28 -0400

    <FEAT> adding error page for non-existing route

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './routes/errorPage.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>
+    element: <App/>,
+    errorElement: <ErrorPage/>
   }
 ])
 
