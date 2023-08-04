faf6f791fa7c5036a9d2b975ea0cd55c20c1e0ec
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-08-04 00:25:20 -0400

    <UPDATE> retriving data from form inputs

--- src/components/sign_up_form/sign_up_form.component.tsx
+++ src/components/sign_up_form/sign_up_form.component.tsx
@@ -1,5 +1,15 @@
+interface FormData {
+  username: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
 export const SignUpForm: React.FC = (): JSX.Element => {
-  const handleSubmit = (): void => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    const formData = Object.fromEntries(new FormData(event.target as HTMLFormElement)) as unknown as FormData
+    console.log(formData)
   }
 
   return (
@@ -7,16 +17,16 @@ export const SignUpForm: React.FC = (): JSX.Element => {
         <h1> Sign up with your email and password </h1>
         <form onSubmit={handleSubmit}>
             <label> Display Name </label>
-            <input type='text' required/>
+            <input name='username' type='text' required/>
                 {/*  */}
             <label> Email </label>
-            <input type='email' required/>
+            <input name='email' type='email' required/>
                 {/*  */}
             <label> Password </label>
-            <input type='password' required/>
+            <input name='password' type='password' required/>
                 {/*  */}
             <label> Confirm Password </label>
-            <input type='password' required/>
+            <input name='confirmPassword' type='password' required/>
                 {/*  */}
             <button type="submit"> Sign Up </button>
         </form>
