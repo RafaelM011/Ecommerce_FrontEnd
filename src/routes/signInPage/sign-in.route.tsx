daa99b52225002530ca2b760dab415d818caa0ec
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-08-04 00:18:57 -0400

    <FEAT> creating sign up form component

--- src/routes/signInPage/sign-in.route.tsx
+++ src/routes/signInPage/sign-in.route.tsx
@@ -1,18 +1,17 @@
-import { type UserAuth } from '../../app.types'
-import { signInWithGooglePopUp, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+// import { type UserAuth } from '../../app.types'
+import { SignUpForm } from '../../components/sign_up_form/sign_up_form.component'
+// import { signInWithGooglePopUp, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 export const SingIn: React.FC = (): JSX.Element => {
-  const logGoogleUser = async (): Promise<void> => {
-    const { user } = await signInWithGooglePopUp()
-    const userDocRef = await createUserDocumentFromAuth(user as UserAuth)
-    console.log(userDocRef)
-  }
+  // const logGoogleUser = async (): Promise<void> => {
+  //   const { user } = await signInWithGooglePopUp()
+  //   const userDocRef = await createUserDocumentFromAuth(user as UserAuth)
+  //   console.log(userDocRef)
+  // }
 
   return (
     <div>
-        I am the sign in route
-        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-        <button onClick={logGoogleUser}> Sign in with google pop-up </button>
+      <SignUpForm/>
     </div>
   )
 }
