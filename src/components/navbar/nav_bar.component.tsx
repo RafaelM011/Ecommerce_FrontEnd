dee183155d9af562cf2939d7488b1583f71e8520
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-08-08 20:04:01 -0400

    setting up the vite svgr plug in to better load svg files

--- src/components/navbar/nav_bar.component.tsx
+++ src/components/navbar/nav_bar.component.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import { UserContext } from '../context/user.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
+import { ReactComponent as CrownIcon } from '../../assets/crown.svg'
+import { ReactComponent as CartIcon } from '../../assets/shopping-bag.svg'
+
 export const NavBar: React.FC = (): JSX.Element => {
   const currentUser = useContext(UserContext)
 
@@ -15,25 +18,7 @@ export const NavBar: React.FC = (): JSX.Element => {
   return (
     <div className="flex justify-between items-center w-full h-[120px]">
         <div className=''>
-            <Link to='/'>
-            {/* <?xml version="1.0" encoding="UTF-8"?> */}
-              <svg width="50px" height="39px" viewBox="0 0 50 39" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                  {/* <!-- Generator: Sketch 53.2 (72643) - https://sketchapp.com --> */}
-                  <title>Group</title>
-                  <desc>Created with Sketch.</desc>
-                  <g id="WiP" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                      <g id="Artboard" transform="translate(-90.000000, -38.000000)">
-                          <g id="Group" transform="translate(90.000000, 38.000000)">
-                              <polygon id="Rectangle" fill="#808282" points="3 14 25 26.5 47 14 40.855176 39 9.08421785 39"></polygon>
-                              <polygon id="Triangle" fillOpacity="0.262838724" fill="#101A1A" points="25 8 40 39 10 39"></polygon>
-                              <circle id="Oval" fill="#5E6363" cx="2" cy="9" r="2"></circle>
-                              <circle id="Oval" fill="#5E6363" cx="25" cy="2" r="2"></circle>
-                              <circle id="Oval" fill="#5E6363" cx="48" cy="9" r="2"></circle>
-                          </g>
-                      </g>
-                  </g>
-              </svg>
-            </Link>
+            <Link to='/'> <CrownIcon/>  </Link>
         </div>
         <div className="flex grow justify-end items-center gap-4">
             <h1> <Link to='/'> HOME </Link> </h1>
@@ -42,7 +27,7 @@ export const NavBar: React.FC = (): JSX.Element => {
               ? <h1> <Link to='/auth'> SIGN IN </Link> </h1>
               : <h1 onClick={handleSignOut}> <Link to='/auth'> SIGN OUT </Link> </h1>
             }
-            <h1> <Link to='/'> ICON </Link> </h1>
+            <CartIcon className='w-[35px]'/>
         </div>
     </div>
   )
