3fcecaa078ab5bb86a5eea83085b1eb5a5c3ddb0
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-08-19 23:48:23 -0400

    addid persistedStore and reducer to redux

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { rootReducer } from './root_reducer'
 import thunk from 'redux-thunk'
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const middleWare = [thunk]
 
-export const store = createStore(rootReducer, undefined, applyMiddleware(...middleWare))
+export const store = createStore(persistedReducer, undefined, applyMiddleware(...middleWare))
+export const persistor = persistStore(store)
 
 // CREATE STORE IS DEPRECATED ON THIS VERSION
